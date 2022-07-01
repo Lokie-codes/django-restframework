00f27b1b3e02f7c7d29df5e2c9d440d518d5b45c
Lokie-codes <lokeshs.19ec@saividya.ac.in>
2022-07-01 06:20:19 +0000

    initial push

---
+++ core/home/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        # fields = ['name', 'age']
+        # exclude = ['id', ]
+        field = '__all__'
